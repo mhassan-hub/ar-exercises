6c2f7a0e6cf29a4bc0aaa4f9159c694a7195f411
Mohamed Hassan <mhass405@gmail.com>
2021-05-25 16:47:33 +0000

    Finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Input a store name!"
+storeName = gets.chomp
+
+input_store = Store.create(name: storeName)
+
+puts input_store.errors.full_messages
